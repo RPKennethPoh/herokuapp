89c8d9e0e193ff24137111a0ebd0bc1c97a58b16
RPKennethPoh <20040654@myrp.edu.sg>
2022-07-07 12:27:01 +0800

    meta gaming

--- web/test.php
+++ web/test.php
@@ -4,6 +4,8 @@
     <title>
         C339 L08 Test
     </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="This is a test PHP page for C339 L08. Done by Kenneth Poh.">
 </head>
