36e827fec6225f42ce21ecf96b846ce2a183dc97
RPKennethPoh <20040654@myrp.edu.sg>
2022-07-07 12:21:41 +0800

    edit test.php

--- web/test.php
+++ web/test.php
@@ -1,5 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <title>
+        C339 L08 Test
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   Hello World!
