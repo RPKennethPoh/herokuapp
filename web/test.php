ab0aeedf068a160e80a13b6170be46321c74ae08
RPKennethPoh <20040654@myrp.edu.sg>
2022-07-07 16:17:09 +0800

    activity 4

--- web/test.php
+++ web/test.php
@@ -1,15 +1,27 @@
+<!-- no doctype -->
 <!DOCTYPE html>
+<!-- html doesn't have lang attribute -->
 <html lang="en">
 <head>
+    <!-- no title -->
     <title>
         C339 L08 Test
     </title>
+    <!-- missing meta data -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- [user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="This is a test PHP page for C339 L08. Done by Kenneth Poh.">
 </head>
 <body>
-  Hello World!
+  <p>Hello World!</p>
+  <!-- Serve images in modern format -->
+  <img src="/img/wallpaper.webp" alt="wallpaper image"/>
+  <!-- Serve responsive images -->
+  <img src="/img/wallpaper.jpg" srcset="/img/wallpaper-small.jpg 100w, /img/wallpaper-small-2x.jpg 200w, /img/wallpaper-1536w.jpg 1536w" sizes="50vw" alt="wallpaper image">
 </body>
+
+
+
 </html>
