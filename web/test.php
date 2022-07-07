f9f5db699731b2e6286800b297e10f7cc059e0b9
RPKennethPoh <20040654@myrp.edu.sg>
2022-07-07 12:24:03 +0800

    edit test.php

--- web/test.php
+++ web/test.php
@@ -5,6 +5,7 @@
         C339 L08 Test
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This is a test PHP page for C339 L08. Done by Kenneth Poh.">
 </head>
 <body>
   Hello World!
